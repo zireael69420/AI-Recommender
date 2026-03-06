1c621e95d3d90a5eb607efcc2c26f78ef5da8a55
zireael69420 <josephcuartel.15@gmail.com>
2026-03-06 17:19:02 +0800

    Initial commit: Setup Django project with MongoDB and AI Recommender

---
+++ store/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from .forms import ProductForm
+from .models import Product
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'store/product_list.html', {'products': products})
+
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm()
+    return render(request, 'store/add_product.html', {'form': form})
